d20989cd85800de15ffc5f05e268ec4f26a3a4b5
suyoooi <100350818+yeoniii20@users.noreply.github.com>
2024-10-16 16:42:33 +0900

    feat: add mock data

---
+++ src/data/dummy.ts
@@ -0,0 +1,82 @@
+import cover1 from "@/app/images/cover/Beatles.png";
+import cover2 from "@/app/images/cover/newjeans.png";
+import cover3 from "@/app/images/cover/newjeans1.png";
+import cover4 from "@/app/images/cover/newjeans2.png";
+import cover5 from "@/app/images/cover/newjeans3.png";
+import cover6 from "@/app/images/cover/newjeans4.png";
+import cover7 from "@/app/images/cover/newjeans5.png";
+import cover8 from "@/app/images/cover/newjeans6.png";
+import cover9 from "@/app/images/cover/newjeans7.png";
+import cover10 from "@/app/images/cover/newjeans8.png";
+import cover11 from "@/app/images/cover/theweekend.png";
+
+export const POPULAR_MUSIC = [
+  {
+    title: "Here Comes The Sun (Remastered 2009)",
+    artist: "Beatles",
+    imageUrl: cover1,
+  },
+  {
+    title: "Bad Girl",
+    artist: "New Jeans",
+    imageUrl: cover2,
+  },
+  {
+    title: "Christmas Carol",
+    artist: "New Jeans",
+    imageUrl: cover3,
+  },
+];
+
+export const RECOMMAND_MUSIC = [
+  {
+    title: "Loveee",
+    artist: "The weekend",
+    imageUrl: cover11,
+  },
+  {
+    title: "Ban HeSuuu",
+    artist: "New Jeans",
+    imageUrl: cover4,
+  },
+  {
+    title: "New Jeans",
+    artist: "New Jeans",
+    imageUrl: cover5,
+  },
+  {
+    title: "Cookie",
+    artist: "New Jeans",
+    imageUrl: cover6,
+  },
+  {
+    title: "Hype Boy",
+    artist: "New Jeans",
+    imageUrl: cover8,
+  },
+  {
+    title: "RAbbit",
+    artist: "New Jeans",
+    imageUrl: cover9,
+  },
+  {
+    title: "Ye",
+    artist: "New Jeans",
+    imageUrl: cover10,
+  },
+  {
+    title: "Here Comes The Sun (Remastered 2009)",
+    artist: "Beatles",
+    imageUrl: cover1,
+  },
+  {
+    title: "Bad Girl",
+    artist: "New Jeans",
+    imageUrl: cover2,
+  },
+  {
+    title: "Christmas Carol",
+    artist: "New Jeans",
+    imageUrl: cover3,
+  },
+];
