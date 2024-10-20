0432c8b894f9b79c8969d0c4f5904abcea00235a
yeoniii20 <commetoi_yeoni@naver.com>
2024-10-20 22:52:54 +0900

    feat: create my page card

---
+++ src/app/components/card/card.tsx
@@ -0,0 +1,23 @@
+const Card = ({
+  icon,
+  title,
+  onClick,
+}: {
+  icon: React.ReactNode;
+  title: string;
+  onClick?: () => void;
+}) => {
+  return (
+    <button
+      className="bg-black-6 p-4 rounded-lg shadow-md hover:shadow-lg transition duration-300"
+      onClick={onClick}
+    >
+      <div className="flex items-center space-x-4">
+        <div className="text-pink-5 text-xl">{icon}</div>
+        <h2 className="text-lg font-semibold">{title}</h2>
+      </div>
+    </button>
+  );
+};
+
+export default Card;
--- src/app/pages/myPage.tsx
+++ src/app/pages/myPage.tsx
@@ -1,5 +1,29 @@
+import {
+  FaUser,
+  FaMusic,
+  FaHeart,
+  FaHeadset,
+  FaInfoCircle,
+  FaBell,
+  FaShoppingCart,
+} from "react-icons/fa";
+import Card from "../components/card/card";
+
 const MyPage = () => {
-  return <></>;
+  return (
+    <div className="bg-black text-gray-1 min-h-screen p-8">
+      <h1 className="text-2xl font-bold mb-8">My Page</h1>
+      <div className="grid grid-cols-1 gap-4 sm:grid-cols-2 lg:grid-cols-3">
+        <Card icon={<FaUser />} title="나의 정보" />
+        <Card icon={<FaShoppingCart />} title="이용권 구매하기" />
+        <Card icon={<FaMusic />} title="좋아하는 노래" />
+        <Card icon={<FaHeart />} title="좋아하는 아티스트" />
+        <Card icon={<FaHeadset />} title="고객센터" />
+        <Card icon={<FaInfoCircle />} title="고객센터" />
+        <Card icon={<FaBell />} title="공지사항" />
+      </div>
+    </div>
+  );
 };
 
 export default MyPage;
