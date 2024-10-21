a30afb69904c05a40d102c34591b801147e1f795
suyoooi <100350818+yeoniii20@users.noreply.github.com>
2024-10-21 14:33:20 +0900

    design: level bedge

--- src/app/pages/myPage.tsx
+++ src/app/pages/myPage.tsx
@@ -4,14 +4,41 @@ import {
   FaHeart,
   FaHeadset,
   FaInfoCircle,
-  FaBell,
   FaShoppingCart,
 } from "react-icons/fa";
 import Image from "next/image";
 import Card from "../components/card/card";
+import { useRouter } from "next/navigation";
 
 const MyPage = () => {
+  const router = useRouter();
   const profileImageUrl: string | null = null;
+
+  // const userLevel = "Bronze";
+  const userLevel = "Silver";
+  // const userLevel = "Gold";
+  // const userLevel = "Diamond";
+  // const userLevel = "";
+
+  const navigateToPurchase = () => {
+    router.push("/");
+  };
+
+  // 레벨에 따른 뱃지 색상 설정
+  const getBadgeColor = (level: string) => {
+    switch (level) {
+      case "Bronze":
+        return "bg-[rgba(205,127,50,0.8)]";
+      case "Silver":
+        return "bg-[rgba(192,192,192,0.8)]";
+      case "Gold":
+        return "bg-[rgba(255,215,0,0.8)]";
+      case "Diamond":
+        return "bg-[rgba(128,0,128,0.8)]";
+      default:
+        return "bg-[rgba(128,128,128,0.8)]";
+    }
+  };
 
   return (
     <div className="bg-black text-gray-1 min-h-screen p-8">
@@ -32,13 +59,23 @@ const MyPage = () => {
         </div>
         <div>
           <h2 className="text-xl font-semibold">뚱이에오</h2>
-          <p className="text-gray-4">나의 레벨: Sliver</p>
+          <span
+            className={`inline-block mt-2 px-3 py-1 text-sm font-medium text-white rounded-full ${getBadgeColor(
+              userLevel
+            )}`}
+          >
+            {userLevel}
+          </span>
         </div>
       </div>
 
       <div className="grid grid-cols-1 gap-4 sm:grid-cols-2 lg:grid-cols-3">
         <Card icon={<FaUser />} title="나의 정보" />
-        <Card icon={<FaShoppingCart />} title="이용권 구매하기" />
+        <Card
+          icon={<FaShoppingCart />}
+          title="이용권 구매하기"
+          onClick={navigateToPurchase}
+        />
         <Card icon={<FaMusic />} title="좋아하는 노래" />
         <Card icon={<FaHeart />} title="좋아하는 아티스트" />
         <Card icon={<FaHeadset />} title="고객센터" />
