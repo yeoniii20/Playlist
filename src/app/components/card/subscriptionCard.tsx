5a3b41a1a42e6543c498f1a6a80e03c9a4274bec
suyoooi <100350818+yeoniii20@users.noreply.github.com>
2024-10-21 14:31:55 +0900

    feat: create subsctiption card component

---
+++ src/app/components/card/subscriptionCard.tsx
@@ -0,0 +1,39 @@
+import React from "react";
+import Button from "../button/button";
+
+interface SubscriptionCardProps {
+  name: string;
+  price: number;
+  benefits: string[];
+  onPurchase: () => void;
+}
+
+const SubscriptionCard = ({
+  name,
+  price,
+  benefits,
+  onPurchase,
+}: SubscriptionCardProps) => {
+  return (
+    <div className="bg-black-6 h-96 min-h-64 p-6 rounded-lg shadow-lg transition duration-300 transform hover:scale-102 flex flex-col justify-between">
+      <div>
+        <h2 className="text-xl font-semibold mb-4 bg-gradient-to-r from-pink-5 to-pink-7 text-transparent bg-clip-text">
+          {name}
+        </h2>
+        <p className="text-lg font-bold mb-4">
+          {price.toLocaleString()}원 / 월
+        </p>
+        <ul className="mb-4">
+          {benefits.map((benefit, index) => (
+            <li key={index} className="text-gray-4 mb-2">
+              - {benefit}
+            </li>
+          ))}
+        </ul>
+      </div>
+      <Button title={"구매하기"} onClick={onPurchase} />
+    </div>
+  );
+};
+
+export default SubscriptionCard;
