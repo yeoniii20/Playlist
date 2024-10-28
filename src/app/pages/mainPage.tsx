4535c0ef0dc588176cc5b9cb1cefa65b61093cf2
yeoniii20 <commetoi_yeoni@naver.com>
2024-10-28 22:33:13 +0900

    refactor: header component

--- src/app/pages/mainPage.tsx
+++ src/app/pages/mainPage.tsx
@@ -5,6 +5,7 @@ import { POPULAR_MUSIC, RECOMMAND_MUSIC } from "@/data/dummy";
 import LargeCard from "../components/card/largeMusicCard";
 import LoginForm from "../components/loginForm";
 import SmallCard from "../components/card/smallMusicCard";
+import Header from "../components/layout/header";
 
 const MainPage = () => {
   const [likedSongs, setLikedSongs] = useState<string[]>([]);
@@ -29,15 +30,7 @@ const MainPage = () => {
 
   return (
     <div className="relative p-8 bg-black-8 min-h-screen text-gray-0">
-      <div className="flex justify-end">
-        <button
-          onClick={() => setIsLoginModalOpen(true)}
-          className="bg-pink-5 text-gray-0 px-4 py-2 rounded-lg"
-        >
-          Login
-        </button>
-      </div>
-
+      <Header onLoginClick={() => setIsLoginModalOpen(true)} />
       {isLoginModalOpen && (
         <div className="fixed inset-0 bg-black-8 bg-opacity-60 flex items-center justify-center z-50">
           <div className="bg-black-7 p-6 rounded-lg shadow-lg relative">
@@ -45,7 +38,6 @@ const MainPage = () => {
           </div>
         </div>
       )}
-
       <h1 className="text-3xl font-bold mb-6 text-pink-4">Music Library</h1>
       <div className="mb-12">
         <h2 className="text-2xl font-bold mb-4 text-pink-3">Recommand Music</h2>
@@ -62,7 +54,6 @@ const MainPage = () => {
           ))}
         </div>
       </div>
-
       <div className="mb-12">
         <h2 className="text-2xl font-bold mb-4 text-pink-3">Popular Charts</h2>
         <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-6">
