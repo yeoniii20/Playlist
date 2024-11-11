7f593d450eda20a4e100630d4c155882ef146bc2
suyoooi <100350818+yeoniii20@users.noreply.github.com>
2024-11-11 18:02:24 +0900

    feat: create list component

---
+++ src/app/components/list/itemList.tsx
@@ -0,0 +1,46 @@
+"use client";
+
+import { getAllItems, deleteItem } from "@/app/api/service/item/api";
+import React, { useEffect, useState } from "react";
+
+const ItemList = () => {
+  const [items, setItems] = useState([]);
+
+  useEffect(() => {
+    fetchItems();
+  }, []);
+
+  const fetchItems = async () => {
+    try {
+      const data = await getAllItems();
+      setItems(data);
+    } catch (error) {
+      console.error("Error fetching items:", error);
+    }
+  };
+
+  const handleDelete = async (id: any) => {
+    try {
+      await deleteItem(id);
+      fetchItems(); // 삭제 후 목록 갱신
+    } catch (error) {
+      console.error("Error deleting item:", error);
+    }
+  };
+
+  return (
+    <div>
+      <h1>아이템 목록</h1>
+      <ul>
+        {items.map((item: any) => (
+          <li key={item.id}>
+            {item.name} - {item.description}
+            <button onClick={() => handleDelete(item.id)}>삭제</button>
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
+};
+
+export default ItemList;
