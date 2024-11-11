2aeca7dd8fcee436e3940378c1b33a8f6e304c03
suyoooi <100350818+yeoniii20@users.noreply.github.com>
2024-11-11 18:01:02 +0900

    feat: add service api

---
+++ src/app/api/service/item/api.ts
@@ -0,0 +1,27 @@
+import axios from "axios";
+
+const API_BASE_URL = "http://localhost:8080/api";
+
+export const createItem = async (item: any) => {
+  const response = await axios.post(`${API_BASE_URL}/items`, item);
+  return response.data;
+};
+
+export const getAllItems = async () => {
+  const response = await axios.get(`${API_BASE_URL}/items`);
+  return response.data;
+};
+
+export const getItemById = async (id: any) => {
+  const response = await axios.get(`${API_BASE_URL}/items/${id}`);
+  return response.data;
+};
+
+export const updateItem = async (id: any, item: any) => {
+  const response = await axios.put(`${API_BASE_URL}/items/${id}`, item);
+  return response.data;
+};
+
+export const deleteItem = async (id: any) => {
+  await axios.delete(`${API_BASE_URL}/items/${id}`);
+};
