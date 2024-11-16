3e987aaa0da6fa8d92f9d98db2972a8665d4485c
yeoniii20 <commetoi_yeoni@naver.com>
2024-11-16 22:18:30 +0900

    feat: add layout

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -11,10 +11,13 @@ import ArtistPage from "./pages/artistPage";
 import EventsPage from "./pages/eventsPage";
 import CreateItemForm from "./components/form/createItemForm";
 import ApiTestPage from "./pages/apiTestPage";
+import NowPlaying from "./components/layout/nowPlaying";
+import Header from "./components/layout/header";
 
 const Page = () => {
   return (
     <div className="px-48 bg-black-8">
+      <Header />
       <ApiTestPage />
       <CreateItemForm />
       <MainPage />
@@ -25,6 +28,7 @@ const Page = () => {
       <MusicDetailPage />
       <ArtistPage />
       <EventsPage />
+      <NowPlaying />
     </div>
   );
 };
